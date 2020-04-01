986a9cb88292ac4775f617587ac74759696d2007
RindiRio95 <48135015+RindiRio95@users.noreply.github.com>
2020-04-01 21:53:40 +0200

    webview tillagd. javascipt enabled i webviewclient. html asset tillagd

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -55,8 +56,10 @@ public class MainActivity extends AppCompatActivity {
            one (1) screenshot showing your external web page.
         */
 
-        WebView webView = new WebView(this);
-        setContentView(webView);
+        WebView myWebView = (WebView) findViewById(R.id.my_webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://his.se");
     }
 
     @Override
